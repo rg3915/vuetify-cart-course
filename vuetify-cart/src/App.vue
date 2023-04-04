c76f8d91968bd6cde40b69b6d95d60a251e8f8d4
rg3915 <regis42santos@gmail.com>
2023-04-04 05:58:20 -0300

    Adiciona colunas.

--- vuetify-cart/src/App.vue
+++ vuetify-cart/src/App.vue
@@ -18,33 +18,43 @@
         </template>
       </v-app-bar>
 
-      <v-main>
+      <v-main class="mt-6">
         <v-container>
-          <v-card
-            flat
-            class="border"
-          >
-            <div class="bg-white px-6 py-2">
-              <v-img src="https://images.kabum.com.br/produtos/fotos/99866/monitor-lg-led-23-8-widescreen-full-hd-ips-hdmi-24mk430h_1547830365_m.jpg"></v-img>
-            </div>
-            <v-card-text>
-              <h4>Monitor LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de Ângulo - 24MK430H</h4>
-
-              <div class="my-4">
-                <div class="font-weight-light text-decoration-line-through mb-n2">R$ 863,62</div>
-                <div class="text-h5 font-weight-bold">R$ 729,99</div>
-                <div class="font-weight-light">À vista no PIX</div>
-              </div>
-
-              <v-btn
+          <v-row>
+            <v-col
+              cols="12"
+              sm="6"
+              md="4"
+              lg="3"
+            >
+              <v-card
                 flat
-                block
-                color="primary"
-              >Adicionar</v-btn>
-            </v-card-text>
-          </v-card>
+                class="border"
+              >
+                <div class="bg-white px-6 py-2">
+                  <v-img src="https://images.kabum.com.br/produtos/fotos/99866/monitor-lg-led-23-8-widescreen-full-hd-ips-hdmi-24mk430h_1547830365_m.jpg"></v-img>
+                </div>
+                <v-card-text>
+                  <h4>Monitor LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de Ângulo - 24MK430H</h4>
+
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 863,62</div>
+                    <div class="text-h5 font-weight-bold">R$ 729,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+
+                  <v-btn
+                    flat
+                    block
+                    color="primary"
+                  >Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-container>
       </v-main>
+
     </v-layout>
   </v-app>
 </template>
