c25de12459fce4f80a356ce24c727e7cf848fd72
rg3915 <regis42santos@gmail.com>
2023-04-04 05:22:41 -0300

    Ajusta a barra superior e troca o tema para Dark.

--- vuetify-cart/src/App.vue
+++ vuetify-cart/src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
+  <v-app theme="dark">
+    <v-layout>
+      <v-app-bar flat density="comfortable" class="text-center border-b">
+        <v-app-bar-title>Vuetify cart</v-app-bar-title>
+      </v-app-bar>
+
+      <v-main>
+        <HelloWorld />
+      </v-main>
+    </v-layout>
   </v-app>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
 </script>
