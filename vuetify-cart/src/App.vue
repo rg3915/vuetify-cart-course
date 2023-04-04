1701068ec1a340cd6baa50db81fdd58be371bb44
rg3915 <regis42santos@gmail.com>
2023-04-04 05:25:44 -0300

    Adiciona icone do carrinho.

--- vuetify-cart/src/App.vue
+++ vuetify-cart/src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <v-app theme="dark">
     <v-layout>
-      <v-app-bar flat density="comfortable" class="text-center border-b">
+      <v-app-bar
+        flat
+        density="comfortable"
+        class="text-center border-b"
+      >
         <v-app-bar-title>Vuetify cart</v-app-bar-title>
+
+        <template #append>
+          <v-badge
+            color="info"
+            dot
+          >
+            <v-icon icon="mdi-cart"></v-icon>
+          </v-badge>
+        </template>
       </v-app-bar>
 
       <v-main>
