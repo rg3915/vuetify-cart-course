0ef9363cb65133a63b57e293f875cad364f6e8dc
rg3915 <regis42santos@gmail.com>
2023-04-08 04:04:24 -0300

    Adiciona Cart.vue

---
+++ vuetify-cart/src/components/cart/Cart.vue
@@ -0,0 +1,13 @@
+<template>
+  <CartEmpty v-if="isEmpty" />
+  <CartProducts v-else />
+</template>
+
+<script setup>
+import { useCart } from '@/composables/useCart'
+import CartEmpty from '@/components/cart/CartEmpty.vue'
+import CartProducts from '@/components/cart/CartProducts.vue'
+
+const { isEmpty } = useCart()
+
+</script>
