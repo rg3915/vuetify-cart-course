8360d750cc07bc8ff7025798100beda7410db54a
rg3915 <regis42santos@gmail.com>
2023-04-08 05:26:03 -0300

    Adiciona ProductLoading.vue

---
+++ vuetify-cart/src/components/product/ProductLoading.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-center">
+    <v-progress-circular
+      indeterminate
+      size="small"
+      width="3"
+    />
+    <br>
+    Carregando produtos
+  </div>
+</template>
